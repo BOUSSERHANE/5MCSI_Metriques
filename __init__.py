0637ca3f9d75f70e7ef547781c69da2e2b55ca52
BOUSSERHANE <151264384+BOUSSERHANE@users.noreply.github.com>
2026-01-08 11:34:47 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -34,7 +34,7 @@ def mongraphique():
 
 @app.route("/histogramme/")
 def monhistogramme():
-    return render_template("histogramme.html")
+    return render_template("Histogramme.html")
 
 if __name__ == "__main__":
   app.run(debug=True)
